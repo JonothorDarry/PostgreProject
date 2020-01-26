3fd2ea05c3612f5416e79ce5a7a69a684db0737d
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-01-26 01:38:45 +0100

    Half of routing, partially finished ins/update

--- flaskk/base.py
+++ flaskk/base.py
@@ -3,17 +3,76 @@ from beatsoup import changer
 
 app = Flask(__name__)
 
+def wanderer(pname):
+    if (pname=='cast'):
+        return 'castles'
+    elif (pname=='met'):
+        return ('metas')
+    elif (pname=='play'):
+        return 'players'
+    return 'armys'
+
 @app.route('/overall.css')
 def css_route():
     fil=open('../apps/overall.css')
     return fil.read() 
 
 
-@app.route('/playersel')
+
+
+@app.route('/metasel', methods = ['POST', 'GET'])
+def metas():
+    if (request.method=='GET'):
+        fil=open('../apps/4meta.html')
+        cf=fil.read()
+        return changer(cf)
+    if (request.method=='POST'):
+        z=request.form['which']
+        return redirect(url_for(wanderer(z)))
+
+@app.route('/castlesel', methods = ['POST', 'GET'])
+def castles():
+    if (request.method=='GET'):
+        fil=open('../apps/3castle.html')
+        cf=fil.read()
+        return changer(cf)
+    if (request.method=='POST'):
+        z=request.form['which']
+        if (z=='ins' or z=='upd' or z=='del'):
+            pass
+
+        return redirect(url_for(wanderer(z)))
+
+
+@app.route('/armysel', methods = ['POST', 'GET'])
+def armys():
+    if (request.method=='GET'):
+        fil=open('../apps/5army.html')
+        cf=fil.read()
+        return changer(cf)
+    if (request.method=='POST'):
+        z=request.form['which']
+        if (z=='ins' or z=='upd' or z=='del'):
+            pass
+
+        return redirect(url_for(wanderer(z)))
+
+
+
+
+@app.route('/playersel', methods = ['POST', 'GET'])
 def players():
-    fil=open('../apps/2player.html')
-    cf=fil.read()
-    return changer(cf)
+    if (request.method=='GET'):
+        fil=open('../apps/2player.html')
+        cf=fil.read()
+        return changer(cf)
+    if (request.method=='POST'):
+        z=request.form['which']
+        if (z=='ins' or z=='upd'):
+            redirect(url_for('playeri'))
+
+        return redirect(url_for(wanderer(z)))
+        
 
 
 
